09aab877ffaabc1579c144b83e3e2aa8e461aa23
Aravind175-cell <avulaaravind00789@gmail.com>
2025-05-08 13:30:20 +0530

    2nd commit

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 const firmRoutes = require('./routes/firmRoutes');
 const productRoutes = require('./routes/productRoutes');
 const cors = require('cors');
-const path = require('path')
 
 const app = express()
 
