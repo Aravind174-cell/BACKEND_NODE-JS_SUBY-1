ad3d440b7517f4d13b642528ce6e05ab61c1c516
Aravind175-cell <avulaaravind00789@gmail.com>
2025-05-11 19:32:39 +0530

    5th commit

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -8,7 +8,8 @@ router.post('/register', vendorController.vendorRegister);
 router.post('/login', vendorController.vendorLogin );
 
 router.get('/all-vendors', vendorController.getAllVendors);
-router.get('/single-vendor/:apple', vendorController.getVendorById);
+// router.get('/single-vendor/:apple', vendorController.getVendorById);
+router.get('/single-vendor/:id', vendorController.getVendorById);
 
 module.exports = router;
 
