472a8a8fa88f38fa37e7bd7cd6fe7ed14713c1bf
Aravind175-cell <avulaaravind00789@gmail.com>
2025-06-03 20:07:34 +0530

    jhguyg

--- routes/sample.js
+++ routes/sample.js
@@ -1,7 +1,3 @@
 console.log("This is created in sample.js File")
 console.log("added temp1")
-<<<<<<< HEAD
 console.log("confirming main")
-=======
-console.log("confirming main")
->>>>>>> 8ff8f08bf3f24d5dc22ed35ed28c740b0e44d6ee
